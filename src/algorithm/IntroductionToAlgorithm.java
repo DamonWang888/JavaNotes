eb0dc407b0096b82fb8e5675cb6e73446a0fa9cf
damonwang888 <1225228598@qq.com>
2021-04-04 20:58:20 +0800

    增加保存并输出切割方案

--- src/algorithm/IntroductionToAlgorithm.java
+++ src/algorithm/IntroductionToAlgorithm.java
@@ -1,12 +1,13 @@
 package algorithm;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class IntroductionToAlgorithm {
 
-
     private static int  Price[]={1,5,8,9,10,17,17,20,24,30};
-
     // 自顶向下递归,给定价格表,钢条长度,求最优切割方案
     // 不声明默认为private 访问权限
+
     int cut_steel_bar(int[] Price, int n){
         int max_price=-1;
         if (n==0)
@@ -17,6 +18,8 @@ public class IntroductionToAlgorithm {
         }
         return max_price;
     }
+
+
 
     // 自低向上
     int cut_steel_bar_(int []Price,int n){
@@ -31,18 +34,51 @@ public class IntroductionToAlgorithm {
     return r[n];
     }
 
-    public static void  main(String[] args){
-        IntroductionToAlgorithm intr=new IntroductionToAlgorithm();
-        int max_benefit=intr.cut_steel_bar_(Price,10);
-        System.out.printf("max benefit %d\n",max_benefit);
-//       System.out.println(new IntroductionToAlgorithm().cut_steel_bar(Price,10));
+    //保存最优切割方案
+    ArrayList<ArrayList<Integer>> extended_bottom_up_cut_rod(int[] price,int n){
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> s=new ArrayList<Integer>(Collections.nCopies(n+1,0));
+        ArrayList<Integer> r=new ArrayList<Integer>(Collections.nCopies(n+1,0));
+
+        for(int j=1;j<=n;j++){
+            Integer i;
+            Integer max_price = -1;
+            for (i = 1; i <= j; i++) {
+                // price 下标从0开始
+                int Cur_Price=Price[i-1] + r.get(j - i);
+                if(max_price<Cur_Price) {
+                    max_price = Cur_Price;
+                    s.set(j, i);
+                }
+            }
+            r.set(j, max_price);
+
+        }
+        res.add(r);
+        res.add(s);
+        return res;
     }
 
+    //打印最优切割方案
+    void print_cut_rod_solution(ArrayList<ArrayList<Integer>> res){
+        int n=res.get(0).size()-1;
+        System.out.printf("n=%d\n",n);
+        ArrayList<Integer> s=res.get(1);
+        while (n>0){
+            Integer cur_length=s.get(n);
+            System.out.printf("cur_length %d\n",cur_length);
+            //System.out.println(cur_length);
+            n=n-cur_length;
+        }
+    }
 
-
-
-
-
-
+    public static void  main(String[] args){
+        IntroductionToAlgorithm intr=new IntroductionToAlgorithm();
+        ArrayList<ArrayList<Integer>> res= intr.extended_bottom_up_cut_rod(Price,10);
+        intr.print_cut_rod_solution(res);
+        // int max_benefit=intr.cut_steel_bar_(Price,10);
+        // System.out.printf("max benefit %d\n",max_benefit)
+        // System.out.println(new IntroductionToAlgorithm().cut_steel_bar(Price,10));
+    }
 
 }
